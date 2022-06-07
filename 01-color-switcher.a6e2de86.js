199fb24bc31247a9afa249ce67db5066838c1cc1
1stPowerLifter <94806144+1stPowerLifter@users.noreply.github.com>
2022-06-07 02:24:00 +0000

    Deploying to gh-pages from @ 1stPowerLifter/goit-js-hw-09@179b89b69bc39b462dcbd66f4462868ae48837b1 🚀

---
+++ 01-color-switcher.a6e2de86.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,o=setInterval((function(){n.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16)))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearTimeout(o)}))}();
+//# sourceMappingURL=01-color-switcher.a6e2de86.js.map
